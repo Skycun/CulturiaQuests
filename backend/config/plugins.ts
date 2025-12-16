265a4c2a544d6679b5876b9e227c58840fc67344
Skycun <ledanoisb@gmail.com>
2025-12-16 20:44:06 +0100

    ❌ Broken commit, cookie automaticly reset.

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,4 +2,11 @@ export default () => ({
   geodata: {
     enabled: true,
   },
+  'users-permissions': {
+    config: {
+      register: {
+        allowedFields: ['age'],
+      },
+    },
+  },
 });
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,19 @@
+<!-- filepath: /home/skycun/cours/CulturiaQuests/frontend/app/pages/index.vue -->
 <template>
-    <main class="text-indigo-500">
-        Hello World!
-    </main>
+  <div>
+    <div v-if="user">
+      <p>Bienvenue, {{ user.username }} !</p>
+      <p>Email : {{ user.email }}</p>
+      <button @click="logout">Se déconnecter</button>
+    </div>
+    <div v-else>
+      <p>Vous n'êtes pas connecté</p>
+      <NuxtLink to="/tests/login">Se connecter</NuxtLink>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const user = useStrapiUser()
+const { logout } = useStrapiAuth()
+</script>
